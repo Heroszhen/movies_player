274e9c6f9aab43a5f98b77574be7e44e1e32f57c
zhen <zhen.yang@hotmail.fr>
2025-01-16 20:15:21 +0100

    add one image in loader

--- assets/react/components/loader/Loader.jsx
+++ assets/react/components/loader/Loader.jsx
@@ -1,6 +1,8 @@
 const Loader = (props) => {
     return (
-        <div id="loader" className="position-absolute top-0 start-0 w-100 h-100 bg-transparent"></div>
+        <div id="loader" className="position-absolute top-0 start-0 w-100 h-100 bg-transparent">
+            <img src="/build/static/loader.webp" className="hero-width-200" />
+        </div>
     );
 }
 export default Loader;
