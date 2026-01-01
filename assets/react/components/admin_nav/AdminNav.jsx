f7a6bd016ec2e04034ff76148d5a76222e762595
zhen <zhen.yang@hotmail.fr>
2026-01-01 12:14:20 +0100

    maj

--- assets/react/components/admin_nav/AdminNav.jsx
+++ assets/react/components/admin_nav/AdminNav.jsx
@@ -6,8 +6,10 @@ import MovieCreationIcon from '@mui/icons-material/MovieCreation';
 import Diversity3Icon from '@mui/icons-material/Diversity3';
 import HomeIcon from '@mui/icons-material/Home';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import WebIcon from '@mui/icons-material/Web';
+import { forwardRef } from 'react';
 
-const AdminNav = ({ toggleAdminNav, ...rest }, ref) => {
+const AdminNav = forwardRef(({ toggleAdminNav }, ref) => {
   return (
     <nav id="admin-nav" className="position-fixed bottom-0 start-0" ref={ref}>
       <NavLink to="/videos" className={({ isActive }) => (isActive ? 'active' : '')}>
@@ -30,11 +32,15 @@ const AdminNav = ({ toggleAdminNav, ...rest }, ref) => {
         <MovieCreationIcon />
         <div>Vidéos</div>
       </NavLink>
+      <NavLink to="/admin/websites" className={({ isActive }) => (isActive ? 'active' : '')}>
+        <WebIcon />
+        <div>Sites</div>
+      </NavLink>
       <a className="hero-cursor-pointer" onClick={() => toggleAdminNav()}>
         <MenuOpenIcon />
         <div>Fermer</div>
       </a>
     </nav>
   );
-};
+});
 export default AdminNav;
