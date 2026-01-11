7d2a788c104c1bbc15662b0bbbf1d5141c8a2e93
zhen <zhen.yang@hotmail.fr>
2026-01-11 19:02:00 +0100

    maj

--- cmds/build.sh
+++ cmds/build.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 # build react
 
+date
+
+mv .env.local dist.env.local
+mv .env.dist.prod.local .env.prod.local
 rm -rf public/build var/cache
+
+echo `pwd`
 npm run build
+
+mv dist.env.local .env.local
+mv .env.prod.local .env.dist.prod.local
+
+date
