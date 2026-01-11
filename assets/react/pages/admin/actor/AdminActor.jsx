8ac8aa367b24536ba5a7f420b376848f96bd27e9
zhen <zhen.yang@hotmail.fr>
2026-01-11 18:50:55 +0100

    maj

--- assets/react/pages/admin/actor/AdminActor.jsx
+++ assets/react/pages/admin/actor/AdminActor.jsx
@@ -20,6 +20,7 @@ import {
   Pagination,
   Grid,
   Input,
+  LinearProgress,
 } from '@mui/material';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import PhotoIcon from '@mui/icons-material/Photo';
@@ -53,6 +54,7 @@ const AdminActor = () => {
   const [orderBy, setOrderBy] = useState('order[id]=asc');
   const [photosToUpload, setPhotosToUpload] = useState([]);
   const [actorPhotos, setActorPhotos] = useState([]);
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
     getPaginator(reactLocation.pathname);
@@ -91,6 +93,7 @@ const AdminActor = () => {
 
       if (type === 3) {
         setActorPhotos([]);
+        setProgress(0);
         setPhotosToUpload([]);
         setActorPhotos(await getPhotoByActorId(actors[index].id));
       }
@@ -131,11 +134,13 @@ const AdminActor = () => {
       newFiles.unshift(files.item(i));
     }
     setPhotosToUpload((prev) => [...newFiles, ...prev]);
+    setProgress(0);
   };
 
   const sendNewPhotos = async () => {
     const options = { actorId: actors[actorIndex].id };
     for (let i = 0; i < photosToUpload.length; i++) {
+      setProgress(Math.round(((i + 1) / photosToUpload.length) * 100));
       const photo = await addFile(photosToUpload[i], options);
       setActorPhotos((prev) => [...prev, photo]);
     }
@@ -326,37 +331,57 @@ const AdminActor = () => {
                 Envoyer
               </Button>
 
-              <Typography variant="h5">Les nouvelles photos</Typography>
-              <Box component="section" className="mb-5 d-flex" sx={{ overflowY: 'auto' }}>
-                {photosToUpload.map((file, index) => {
-                  return (
-                    <span className="pe-3" key={index}>
-                      <img src={URL.createObjectURL(file)} alt="" style={{ width: 400 + 'px' }} />
-                      <DeleteForeverIcon
-                        onClick={() => setPhotosToUpload((prev) => prev.filter((_, prevIndex) => index !== prevIndex))}
-                        className="hero-cursor-pointer mb-1 d-block"
-                      />
-                    </span>
-                  );
-                })}
-              </Box>
+              {photosToUpload.length > 0 && (
+                <>
+                  <Typography variant="h5">Les nouvelles photos</Typography>
+                  <Box sx={{ mt: 2 }}>
+                    <Box sx={{ minWidth: 35 }}>
+                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        {progress}
+                      </Typography>
+                    </Box>
+                    <Box sx={{ width: '100%', mb: 3 }}>
+                      <LinearProgress variant="determinate" value={progress} sx={{ height: 10, borderRadius: 5 }} />
+                    </Box>
+                  </Box>
+                  <Box component="section" className="mb-5 d-flex" sx={{ overflowY: 'auto' }}>
+                    {photosToUpload.map((file, index) => {
+                      return (
+                        <span className="pe-3" key={index}>
+                          <img src={URL.createObjectURL(file)} alt="" style={{ width: 400 + 'px' }} />
+                          <DeleteForeverIcon
+                            onClick={() =>
+                              setPhotosToUpload((prev) => prev.filter((_, prevIndex) => index !== prevIndex))
+                            }
+                            className="hero-cursor-pointer mb-1 d-block"
+                          />
+                        </span>
+                      );
+                    })}
+                  </Box>
+                </>
+              )}
 
-              <Box component="section" className="mb-3">
-                <Typography variant="h5">Les photos</Typography>
-                <Grid container spacing={1}>
-                  {actorPhotos.map((photo, index) => {
-                    return (
-                      <Grid key={index} item xs={6} sm={4} md={3} lg={2} sx={{ mb: 3, ps: 1, pe: 1 }}>
-                        <img src={`${process.env.AWS_FILE_PREFIX}${photo.imageName}`} alt="" />
-                        <DeleteForeverIcon
-                          onClick={() => deleteOldPhoto(index)}
-                          className="hero-cursor-pointer mb-1 d-block"
-                        />
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-              </Box>
+              {actorPhotos.length > 0 && (
+                <>
+                  <Box component="section" className="mb-3">
+                    <Typography variant="h5">Les photos</Typography>
+                    <Grid container spacing={1}>
+                      {actorPhotos.map((photo, index) => {
+                        return (
+                          <Grid key={index} item xs={6} sm={4} md={3} lg={2} sx={{ mb: 3, ps: 1, pe: 1 }}>
+                            <img src={`${process.env.AWS_FILE_PREFIX}${photo.imageName}`} alt="" />
+                            <DeleteForeverIcon
+                              onClick={() => deleteOldPhoto(index)}
+                              className="hero-cursor-pointer mb-1 d-block"
+                            />
+                          </Grid>
+                        );
+                      })}
+                    </Grid>
+                  </Box>
+                </>
+              )}
             </section>
           )}
         </Box>
