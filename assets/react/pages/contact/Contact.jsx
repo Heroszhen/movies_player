7e73d8bfa3ba2497eb1dfcab5bba297b61ddd809
zhen <zhen.yang@hotmail.fr>
2026-05-17 10:29:50 +0200

    maj

--- assets/react/pages/contact/Contact.jsx
+++ assets/react/pages/contact/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
     <section id="contact" className="min-vh-100 hero-fs-40 md:hero-fs-60 lg:hero-fs-120 hover:hero-fs-200">
-      contact
+      En construction
     </section>
   );
 };
