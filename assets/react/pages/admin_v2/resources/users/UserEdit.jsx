43e6498ba44d158a35d5cf1fb17a6de8b9cbe279
zhen <zhen.yang@hotmail.fr>
2026-08-20 16:49:00 +0200

    maj

--- assets/react/pages/admin_v2/resources/users/UserEdit.jsx
+++ assets/react/pages/admin_v2/resources/users/UserEdit.jsx
@@ -1,9 +1,14 @@
 import { Edit, SimpleForm } from 'react-admin';
 import { UserFormFields } from './UserFormFields';
 
+const transform = (data) => {
+  const { photo, ...rest } = data;
+  return rest;
+};
+
 export const UserEdit = () => {
   return (
-    <Edit mutationMode="pessimistic">
+    <Edit mutationMode="pessimistic" transform={transform}>
       <SimpleForm>
         <UserFormFields />
       </SimpleForm>
