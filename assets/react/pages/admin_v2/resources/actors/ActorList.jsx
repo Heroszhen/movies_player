7c536b33336e5dcbd1ef54b5cbeca31b38500173
zhen <zhen.yang@hotmail.fr>
2026-08-23 19:50:06 +0200

    maj

--- assets/react/pages/admin_v2/resources/actors/ActorList.jsx
+++ assets/react/pages/admin_v2/resources/actors/ActorList.jsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Datagrid,
   EditButton,
   FunctionField,
@@ -12,11 +13,12 @@ import {
 import PhotoIcon from '@mui/icons-material/Photo';
 import CollectionsIcon from '@mui/icons-material/Collections';
 import { useState } from 'react';
-import { Box, Modal, Typography } from '@mui/material';
+import { Box, Grid, Input, LinearProgress, Modal, Typography } from '@mui/material';
 import { getModalStyle } from '../../../../services/data';
 import { PhotoEdit } from '../common/PhotoEdit';
-import { deletePhoto } from '../../../../stores/fileStore';
+import { deletePhoto, addFile, getPhotoByActorId } from '../../../../stores/fileStore';
 import { wait } from '../../../../services/utils';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export const ActorList = () => {
   const filters = [
@@ -27,12 +29,25 @@ export const ActorList = () => {
   const [open, setOpen] = useState(false);
   const [formType, setFormType] = useState(null);
   const [update] = useUpdate();
+  const [photosToUpload, setPhotosToUpload] = useState([]);
+  const [actorPhotos, setActorPhotos] = useState([]);
+  const [progress, setProgress] = useState(0);
 
-  const toggleModal = (newFormType = null, actor = null) => {
+  const toggleModal = async (newFormType = null, actor = null) => {
     setActorToModify(actor);
     setFormType(newFormType);
-    if (newFormType === null) setOpen(false);
-    else setOpen(true);
+    if (newFormType === null) {
+      setOpen(false);
+      return;
+    }
+    setOpen(true);
+
+    if (newFormType === 2) {
+      setActorPhotos([]);
+      setProgress(0);
+      setPhotosToUpload([]);
+      setActorPhotos(await getPhotoByActorId(actor.id));
+    }
   };
 
   const modifyPhoto = async (newPhoto) => {
@@ -47,6 +62,32 @@ export const ActorList = () => {
       await wait(0.5);
       if (oldPhotoId) await deletePhoto(oldPhotoId);
     }
+  };
+
+  const handleActorPhotos = (e) => {
+    const files = e.target.files;
+    const newFiles = [];
+    for (let i = files.length - 1; i >= 0; i--) {
+      newFiles.unshift(files.item(i));
+    }
+    setPhotosToUpload((prev) => [...newFiles, ...prev]);
+    setProgress(0);
+  };
+
+  const sendNewPhotos = async () => {
+    const options = { actorId: actorToModify.id };
+    for (let i = 0; i < photosToUpload.length; i++) {
+      setProgress(Math.round(((i + 1) / photosToUpload.length) * 100));
+      const photo = await addFile(photosToUpload[i], options);
+      setActorPhotos((prev) => [...prev, photo]);
+    }
+
+    setPhotosToUpload([]);
+  };
+
+  const deleteOldPhoto = async (index) => {
+    await deletePhoto(actorPhotos[index].id);
+    setActorPhotos((prev) => prev.filter((_, prevIndex) => prevIndex !== index));
   };
 
   return (
@@ -74,7 +115,7 @@ export const ActorList = () => {
               <>
                 <EditButton className="me-1" />
                 <PhotoIcon className="cursor-pointer me-1" onClick={() => toggleModal(1, record)} />
-                <CollectionsIcon className="cursor-pointer me-1" />
+                <CollectionsIcon className="cursor-pointer me-1" onClick={() => toggleModal(2, record)} />
               </>
             )}
           />
@@ -83,16 +124,93 @@ export const ActorList = () => {
 
       <Modal
         open={open}
-        onClose={() => setOpen(false)}
+        onClose={() => toggleModal()}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description">
         <Box sx={getModalStyle(formType === 1 ? 500 : '90%')}>
           {formType === 1 && (
             <>
               <Typography id="modal-modal-title" variant="h5" component="h2" sx={{ mb: 4 }}>
-                Editer la photo de l&lsquoacteur {actorToModify?.name}
+                Editer la photo de {actorToModify?.name}
               </Typography>
               <PhotoEdit modifyPhoto={modifyPhoto} />
+            </>
+          )}
+          {formType === 2 && (
+            <>
+              <Typography id="modal-modal-title" variant="h5" component="h2" sx={{ mb: 4 }}>
+                Editer les photos de {actorToModify?.name}
+              </Typography>
+              <Box component="section" className="mb-3">
+                <Input
+                  onChange={(e) => handleActorPhotos(e)}
+                  type="file"
+                  id="upload-photos"
+                  inputProps={{ accept: 'image/*', multiple: true }}
+                  fullWidth="true"
+                  className="mb-2"
+                />
+                <Button
+                  variant="contained"
+                  type="button"
+                  disabled={photosToUpload.length === 0}
+                  sx={{ mb: 2 }}
+                  onClick={() => sendNewPhotos()}>
+                  Envoyer
+                </Button>
+              </Box>
+
+              {photosToUpload.length > 0 && (
+                <>
+                  <Typography variant="h5">Les nouvelles photos</Typography>
+                  <Box sx={{ mt: 2 }}>
+                    <Box sx={{ minWidth: 35 }}>
+                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        {progress}%
+                      </Typography>
+                    </Box>
+                    <Box sx={{ width: '100%', mb: 3 }}>
+                      <LinearProgress variant="determinate" value={progress} sx={{ height: 10, borderRadius: 5 }} />
+                    </Box>
+                  </Box>
+                  <Box component="section" className="mb-5 d-flex" sx={{ overflowY: 'auto' }}>
+                    {photosToUpload.map((file, index) => {
+                      return (
+                        <span className="pe-3" key={index}>
+                          <img src={URL.createObjectURL(file)} alt="" style={{ width: 400 + 'px' }} />
+                          <DeleteForeverIcon
+                            onClick={() =>
+                              setPhotosToUpload((prev) => prev.filter((_, prevIndex) => index !== prevIndex))
+                            }
+                            className="hero-cursor-pointer mb-1 d-block"
+                          />
+                        </span>
+                      );
+                    })}
+                  </Box>
+                </>
+              )}
+
+              {actorPhotos.length > 0 && (
+                <>
+                  <Box component="section" className="mb-3">
+                    <Typography variant="h5">Les photos</Typography>
+                    <Grid container spacing={1}>
+                      {actorPhotos.map((photo, index) => {
+                        return (
+                          <Grid key={index} item xs={6} sm={4} md={3} lg={2} sx={{ mb: 3, ps: 1, pe: 1 }}>
+                            <img src={`${process.env.AWS_FILE_PREFIX}${photo.imageName}`} alt="" />
+                            <DeleteForeverIcon
+                              onClick={() => deleteOldPhoto(index)}
+                              className="hero-cursor-pointer mb-1 d-block"
+                            />
+                          </Grid>
+                        );
+                      })}
+                    </Grid>
+                  </Box>
+                </>
+              )}
             </>
           )}
         </Box>
